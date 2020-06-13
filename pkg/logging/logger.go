c6cca76b3e1563a2d50f3e4e31e5b3e362085a74
Henry Zhang <sunnywalden@gmail.com>
2020-06-13 17:22:55 +0800

    log using configure loglevel

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -3,14 +3,19 @@ package logging
 import (
 
 	"github.com/sirupsen/logrus"
+
+	"github.com/sunnywalden/sync-data/config"
 )
 
 
 func GetLogger() (logger *logrus.Logger) {
 
+	configures := config.Conf
+	logLevel := configures.Log.Level
+
 	logger = logrus.New()
 	logger.Formatter = &logrus.JSONFormatter{}
-	logger.Level = logrus.DebugLevel
+	logger.Level = logLevel
 
 	return logger
 }
